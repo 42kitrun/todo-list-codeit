1f7c3623a56f793372dca8b481326a87fe35b609
42kitrun <42kit.run@gmail.com>
2025-06-10 00:38:58 +0900

    6월9일 1일차

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4285f4",
+        secondary: "#34a853",
+        accent: "#fbbc05",
+        danger: "#ea4335",
+        // 필요에 따라 추가
+      },
+    },
+  },
+  // content, plugins 등 기본 설정
+};
